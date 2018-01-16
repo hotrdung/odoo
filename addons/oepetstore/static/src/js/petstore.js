8ab7571d518a349bbfb36bf3957c3067ba1c6369
Trung Dung Ho <dung.ht@teko.vn>
2018-01-16 16:40:38 +0700

    complete simple Widget

--- addons/oepetstore/static/src/js/petstore.js
+++ addons/oepetstore/static/src/js/petstore.js
@@ -4,11 +4,31 @@ odoo.define('PetStoreHomePage', function (require) {
     var core = require('web.core');
     var Widget = require('web.Widget');
 
+    var Class = require('web.Class');
+
+    var MyClass = Class.extend({
+        init: function (name) {
+            this.name = name;
+        },
+        say_hello: function () {
+            console.log("Hello ", this.name);
+        },
+    });
+
+    var MySpanishClass = MyClass.extend({
+        say_hello: function () {
+            this._super();
+            console.log("[Spanish] Hola ", this.name);
+        },
+    });
+
     var HomePageWidget = Widget.extend({
         //template: 'petstore.homepage',
         start: function () {
-            // this.$el.append($('<div>').text('Hello dear Odoo user!'));
-            console.log("Pet Store HomePage widget start");
+            this.$el.append($('<div>').text('Welcome to Pet Store 333!'));
+            // console.log("xxx");
+            var my_object = new MySpanishClass("Pet Store");
+            my_object.say_hello();
         }
     });
 
