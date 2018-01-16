fdeb860b7e7e02f7f7997a758bed6085f9def6b8
Trung Dung Ho <dung.ht@teko.vn>
2018-01-16 09:52:29 +0700

    complete `website`

---
+++ addons/academy/models/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+    biography = fields.Html()
+
+    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+
+class Courses(models.Model):
+    _name = 'academy.courses'
+    _inherit = 'mail.thread'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
